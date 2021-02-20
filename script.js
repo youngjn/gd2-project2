f65821ce15119a192ade9781a609575040686275
youngjn <gkgkgk98@gmail.com>
2021-02-20 03:36:46 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,171 @@
-var a=10;
-var b=20;
-var result;
+
+$(window).bind("load resize scroll",function(e) {
+    var y = $(window).scrollTop();
+ 
+    $(".space-1").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' + parseInt(-y / 6) + 'px');
+    $(".aquarius").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' +  parseInt(-y / 9) + 'px');
+    $(".pisces").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' + parseInt(-y / 4) + 'px');
 
 
 
-calculate(a,b);
-console.log(result);
+    $(".space-2").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' + parseInt(-y / 6 + 250) + 'px');
+    $(".aries").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' +  parseInt(-y / 9 + 150) + 'px');
+    $(".taurus").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' + parseInt(-y / 4 + 320) + 'px');
 
 
-function calculate(x,y) {
-	result = x+y;
-	return result;
+
+    $(".space-3").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' + parseInt(-y / 6 + 500) + 'px');
+    $(".gemini").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' +  parseInt(-y / 9 + 300) + 'px');
+    $(".cancer").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' + parseInt(-y / 4 + 640) + 'px');
+
+
+
+    $(".space-4").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' + parseInt(-y / 6 + 750) + 'px');
+    $(".leo").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' +  parseInt(-y / 9 + 430) + 'px');
+    $(".virgo").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' + parseInt(-y / 4 + 960) + 'px');
+
+
+
+    $(".space-5").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' + parseInt(-y / 6 + 1000) + 'px');
+    $(".libra").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' +  parseInt(-y / 9 + 580) + 'px');
+    $(".scorpio").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' + parseInt(-y / 4 + 1280) + 'px');
+
+
+
+    $(".space-6").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' + parseInt(-y / 6 + 1250) + 'px');
+    $(".sagittarius").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' +  parseInt(-y / 9 + 720) + 'px');
+    $(".capricon").filter(function() {
+        return $(this).offset().top < (y + $(window).height()) &&
+               $(this).offset().top + $(this).height() > y;
+    }).css('background-position', '0px ' + parseInt(-y / 4 + 1600) + 'px');
+
+});
+
+function myFunction() {
+
+	var star = document.querySelector('.menu');
+	if(star.style.display === "block") {
+		star.style.display = "none";
+	} else {
+		star.style.display = "block";
+	}
 }
+
+function dragMoveListener (event) {
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  // translate the element
+  target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)'
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
+
+
+interact('.dropzone').dropzone({
+  // only accept elements matching this CSS selector
+  accept: '#yes-drop',
+  // Require a 75% element overlap for a drop to be possible
+  overlap: 0.75,
+
+  // listen for drop related events:
+
+  ondropactivate: function (event) {
+    // add active dropzone feedback
+    event.target.classList.add('drop-active')
+  },
+  ondragenter: function (event) {
+    var draggableElement = event.relatedTarget
+    var dropzoneElement = event.target
+
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add('drop-target')
+    draggableElement.classList.add('can-drop')
+    draggableElement.textContent = 'Dragged in'
+  },
+  ondragleave: function (event) {
+    // remove the drop feedback style
+    event.target.classList.remove('drop-target')
+    event.relatedTarget.classList.remove('can-drop')
+    event.relatedTarget.textContent = 'Dragged out'
+  },
+  ondrop: function (event) {
+    console.log("Dropped");
+  },
+  ondropdeactivate: function (event) {
+    // remove active dropzone feedback
+    event.target.classList.remove('drop-active')
+    event.target.classList.remove('drop-target')
+  }
+})
+
+
+interact('.drag-drop')
+  .draggable({
+    inertia: true,
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: '.section.dropzone'
+      })
+    ],
+    autoScroll: true,
+    // dragMoveListener from the dragging demo above
+    listeners: { move: dragMoveListener }
+  })
