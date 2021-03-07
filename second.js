6483b256f50988da386960d2985b0c1f08c0bc1a
youngjn <gkgkgk98@gmail.com>
2021-03-07 01:38:52 -0700

    Add files via upload

--- second.js
+++ second.js
@@ -1,80 +1,30 @@
-$(window).on('load', function(){
-  setTimeout(removeLoader, 2300); //wait for page load PLUS two seconds.
-});
-function removeLoader(){
-    $( "#loading" ).fadeOut(100, function() {
-      // fadeOut complete. Remove the loading div
-      $( "#loading" ).remove(); //makes page more lightweight 
-  });  
-}
+
 
 var n = 0;
 var i = 0;
 var k = 0;
 var eee = 0;
 
-// $('.timesquare1').click(function(e) {
-//   var mouseX = e.clientX;
-//   var mouseY = e.clientY;
-//   if(mouseX >= 666 && mouseX <= 689 && mouseY >= 450 && mouseY <= 486) {
-
-//     console.log("clicked");
-//     $('.findN h3').css("display", "block");
-//   } else if(mouseX >= 694 && mouseX <= 699 && mouseY >= 764 && mouseY <= 779) {
-//     $('.findI h3').css("display", "block");
-//   } else if(mouseX >= 1060 && mouseX <= 1080 && mouseY >= 854 && mouseY <= 869) {
-//     $('.findK h3').css("display", "block");
-//   } else if(mouseX >= 935 && mouseX <= 947 && mouseY >= 623 && mouseY <= 643) {
-//     $('.findE h3').css("display", "block");
-//   }
-// });
-
 function nextPage() {
-  window.location.href = 'second.html';
+  window.location.href = 'third.html';
 }
 
-$('.timesquare1').click(function(e) {
+
+
+$(".shibuya1").click(function(e) {
   var target = e.target;
   var rect = target.getBoundingClientRect();
   var x = e.clientX - rect.left;
   var y = e.clientY - rect.top;
   console.log(x + " , " + y);
-  if(x > 665 && x < 691) {
+  if(x > 740 && x < 751) {
     console.log("x is satisfied");
-    if (y > 410 && y < 453) {
-
-    console.log("clicked");
-    $('.findN h3').css("display", "block");
-    n++;
-    } else {
-      alert("Getting close");
-    }
-  } else {
-    alert("Not even close");
-  }
-
-if(n >= 1 && i >= 1 && k >= 1 && eee >= 1) {
-
-  alert("You found everything! Wait for the next stage!");
-  setTimeout(nextPage, 1000);
-}
-
-});
-
-$(".timesquare2").click(function(e) {
-  var target = e.target;
-  var rect = target.getBoundingClientRect();
-  var x = e.clientX - rect.left;
-  var y = e.clientY - rect.top;
-  console.log(x + " , " + y);
-  if(x > 690 && x < 700) {
-    console.log("x is satisfied");
-    if (y > 725 && y < 740) {
+    if (y > 638 && y < 657) {
 
     console.log("clicked");
     $('.findI h3').css("display", "block");
     i++;
-    } else {
+    } else{
       alert("Getting close");
     }
   } else {
@@ -86,25 +36,18 @@ $(".timesquare2").click(function(e) {
   alert("You found everything! Wait for the next stage!");
   setTimeout(nextPage, 1000);
 }
+
 });
 
-$(".timesquare3").click(function(e) {
-  var target = e.target;
-  var rect = target.getBoundingClientRect();
-  var x = e.clientX - rect.left;
-  var y = e.clientY - rect.top;
-  alert("Not even close");
-});
-
-$(".timesquare4").click(function(e) {
+$(".shibuya2").click(function(e) {
   var target = e.target;
   var rect = target.getBoundingClientRect();
   var x = e.clientX - rect.left;
   var y = e.clientY - rect.top;
   console.log(x + " , " + y);
-  if(x > 815 && x < 830) {
+  if(x > 364 && x < 381) {
     console.log("x is satisfied");
-    if (y > 588 && y < 609) {
+    if (y > 388 && y < 421) {
 
     console.log("clicked");
     $('.findE h3').css("display", "block");
@@ -121,17 +64,27 @@ $(".timesquare4").click(function(e) {
   alert("You found everything! Wait for the next stage!");
   setTimeout(nextPage, 1000);
 }
+
 });
 
-$(".timesquare5").click(function(e) {
+$(".shibuya3").click(function(e) {
   var target = e.target;
   var rect = target.getBoundingClientRect();
   var x = e.clientX - rect.left;
   var y = e.clientY - rect.top;
   console.log(x + " , " + y);
-  if(x > 40 && x < 60) {
+  alert("Not even close");
+});
+
+$(".shibuya4").click(function(e) {
+  var target = e.target;
+  var rect = target.getBoundingClientRect();
+  var x = e.clientX - rect.left;
+  var y = e.clientY - rect.top;
+  console.log(x + " , " + y);
+  if(x > 122 && x < 141) {
     console.log("x is satisfied");
-    if (y > 815 && y < 831) {
+    if (y > 169 && y < 191) {
 
     console.log("clicked");
     $('.findK h3').css("display", "block");
@@ -148,5 +101,43 @@ $(".timesquare5").click(function(e) {
   alert("You found everything! Wait for the next stage!");
   setTimeout(nextPage, 1000);
 }
+
 });
 
+$(".shibuya5").click(function(e) {
+  var target = e.target;
+  var rect = target.getBoundingClientRect();
+  var x = e.clientX - rect.left;
+  var y = e.clientY - rect.top;
+  console.log(x + " , " + y);
+  alert("Not even close");
+});
+
+$(".shibuya6").click(function(e) {
+  var target = e.target;
+  var rect = target.getBoundingClientRect();
+  var x = e.clientX - rect.left;
+  var y = e.clientY - rect.top;
+  console.log(x + " , " + y);
+  if(x > 446 && x < 459) {
+    console.log("x is satisfied");
+    if (y > 637 && y < 649) {
+
+    console.log("clicked");
+    $('.findN h3').css("display", "block");
+    n++;
+    } else{
+      alert("Getting close");
+    }
+  } else {
+    alert("Not even close");
+  }
+
+
+  if(n >= 1 && i >= 1 && k >= 1 && eee >= 1) {
+
+  alert("You found everything! Wait for the next stage!");
+  setTimeout(nextPage, 1000);
+}
+
+});
--- third.js
+++ third.js
@@ -1,80 +1,69 @@
-$(window).on('load', function(){
-  setTimeout(removeLoader, 2300); //wait for page load PLUS two seconds.
-});
-function removeLoader(){
-    $( "#loading" ).fadeOut(100, function() {
-      // fadeOut complete. Remove the loading div
-      $( "#loading" ).remove(); //makes page more lightweight 
-  });  
-}
+
 
 var n = 0;
 var i = 0;
 var k = 0;
 var eee = 0;
 
-// $('.timesquare1').click(function(e) {
-//   var mouseX = e.clientX;
-//   var mouseY = e.clientY;
-//   if(mouseX >= 666 && mouseX <= 689 && mouseY >= 450 && mouseY <= 486) {
-
-//     console.log("clicked");
-//     $('.findN h3').css("display", "block");
-//   } else if(mouseX >= 694 && mouseX <= 699 && mouseY >= 764 && mouseY <= 779) {
-//     $('.findI h3').css("display", "block");
-//   } else if(mouseX >= 1060 && mouseX <= 1080 && mouseY >= 854 && mouseY <= 869) {
-//     $('.findK h3').css("display", "block");
-//   } else if(mouseX >= 935 && mouseX <= 947 && mouseY >= 623 && mouseY <= 643) {
-//     $('.findE h3').css("display", "block");
-//   }
-// });
-
 function nextPage() {
-  window.location.href = 'second.html';
+  $('.end').css("display", "block");
 }
 
-$('.timesquare1').click(function(e) {
+
+
+$(".london1").click(function(e) {
   var target = e.target;
   var rect = target.getBoundingClientRect();
   var x = e.clientX - rect.left;
   var y = e.clientY - rect.top;
   console.log(x + " , " + y);
-  if(x > 665 && x < 691) {
+  if(x > 249 && x < 261) {
     console.log("x is satisfied");
-    if (y > 410 && y < 453) {
+    if (y > 79 && y < 92) {
 
     console.log("clicked");
-    $('.findN h3').css("display", "block");
-    n++;
-    } else {
+    $('.findK h3').css("display", "block");
+    k++;
+    } else{
       alert("Getting close");
     }
   } else {
     alert("Not even close");
   }
 
-if(n >= 1 && i >= 1 && k >= 1 && eee >= 1) {
+  if(n >= 1 && i >= 1 && k >= 1 && eee >= 1) {
 
-  alert("You found everything! Wait for the next stage!");
+  alert("You found everything! Wait for the next step!");
   setTimeout(nextPage, 1000);
 }
 
 });
 
-$(".timesquare2").click(function(e) {
+$(".london2").click(function(e) {
   var target = e.target;
   var rect = target.getBoundingClientRect();
   var x = e.clientX - rect.left;
   var y = e.clientY - rect.top;
   console.log(x + " , " + y);
-  if(x > 690 && x < 700) {
+  alert("Not even close");
+
+
+});
+
+$(".london3").click(function(e) {
+  var target = e.target;
+  var rect = target.getBoundingClientRect();
+  var x = e.clientX - rect.left;
+  var y = e.clientY - rect.top;
+  console.log(x + " , " + y);
+  if(x > 485 && x < 494) {
     console.log("x is satisfied");
-    if (y > 725 && y < 740) {
+    if (y > 96 && y < 111) {
 
     console.log("clicked");
     $('.findI h3').css("display", "block");
     i++;
-    } else {
+    } else{
       alert("Getting close");
     }
   } else {
@@ -86,25 +75,29 @@ $(".timesquare2").click(function(e) {
   alert("You found everything! Wait for the next stage!");
   setTimeout(nextPage, 1000);
 }
+
 });
 
-$(".timesquare3").click(function(e) {
-  var target = e.target;
-  var rect = target.getBoundingClientRect();
-  var x = e.clientX - rect.left;
-  var y = e.clientY - rect.top;
-  alert("Not even close");
-});
-
-$(".timesquare4").click(function(e) {
+$(".london4").click(function(e) {
   var target = e.target;
   var rect = target.getBoundingClientRect();
   var x = e.clientX - rect.left;
   var y = e.clientY - rect.top;
   console.log(x + " , " + y);
-  if(x > 815 && x < 830) {
+  alert("Not even close");
+
+
+});
+
+$(".london5").click(function(e) {
+  var target = e.target;
+  var rect = target.getBoundingClientRect();
+  var x = e.clientX - rect.left;
+  var y = e.clientY - rect.top;
+  console.log(x + " , " + y);
+  if(x > 566 && x < 581) {
     console.log("x is satisfied");
-    if (y > 588 && y < 609) {
+    if (y > 712 && y < 731) {
 
     console.log("clicked");
     $('.findE h3').css("display", "block");
@@ -121,21 +114,22 @@ $(".timesquare4").click(function(e) {
   alert("You found everything! Wait for the next stage!");
   setTimeout(nextPage, 1000);
 }
+
 });
 
-$(".timesquare5").click(function(e) {
+$(".london6").click(function(e) {
   var target = e.target;
   var rect = target.getBoundingClientRect();
   var x = e.clientX - rect.left;
   var y = e.clientY - rect.top;
   console.log(x + " , " + y);
-  if(x > 40 && x < 60) {
+  if(x > 539 && x < 571) {
     console.log("x is satisfied");
-    if (y > 815 && y < 831) {
+    if (y > 429 && y < 454) {
 
     console.log("clicked");
-    $('.findK h3').css("display", "block");
-    k++;
+    $('.findN h3').css("display", "block");
+    n++;
     } else{
       alert("Getting close");
     }
@@ -148,5 +142,5 @@ $(".timesquare5").click(function(e) {
   alert("You found everything! Wait for the next stage!");
   setTimeout(nextPage, 1000);
 }
-});
 
+});
